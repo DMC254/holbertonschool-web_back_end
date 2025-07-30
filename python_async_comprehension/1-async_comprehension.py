76cc95c03e6272c7dcdc5a7509ac961677e73f15
Diego <mercadodiego2006@gmail.com>
2025-07-30 15:07:34 -0400

    task 1

---
+++ python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+"""Async Comprehension module.
+
+This module defines an asynchronous comprehension function that returns
+a list of random numbers.
+"""
+
+import asyncio
+from typing import List
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehesion() -> List[float]:
+    """Asynchronous comprehension function.
+
+    This coroutine will asynchronously generate a list of random numbers
+    using the async_generator coroutine.
+    """
+    return [i async for i in async_generator()]
